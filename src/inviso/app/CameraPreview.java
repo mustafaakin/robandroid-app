ebfbdbc17cacf4f80232b72f08dda33bea85d958
Mustafa Akin <mustafa91@gmail.com>
2013-03-17 23:22:38 +0200

    - Camera stop/start functions working. - Application logging out correctly - Command codes changed to <128 values because of type conversion stuff.

--- src/inviso/app/CameraPreview.java
+++ src/inviso/app/CameraPreview.java
@@ -30,6 +30,15 @@ public class CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCall
 	long previous = 0;
 	ConnectionChannel channel; 
 	
+	public void stopCamera(){
+		mCamera.stopPreview();
+	}
+	
+	public void startCamera(){
+		mCamera.startPreview();
+	}
+	
+	
 	public void setChannel(ConnectionChannel channel) {
 		this.channel = channel;
 	}
@@ -37,7 +46,6 @@ public class CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCall
 	public CameraPreview(int PreviewlayoutWidth, int PreviewlayoutHeight) {
 		Camera.CameraInfo info = new Camera.CameraInfo();
 		cameraId = info.facing;
-
 		
 		PreviewSizeWidth = PreviewlayoutWidth;
 		PreviewSizeHeight = PreviewlayoutHeight;
@@ -52,82 +60,15 @@ public class CameraPreview implements SurfaceHolder.Callback, Camera.PreviewCall
 
 			int width  = 640;
 			int height = 480;
-					
-			long t1 = System.currentTimeMillis();
-			
+								
 			ByteArrayOutputStream outstr = new ByteArrayOutputStream();
 			Rect rect = new Rect(0, 0, width, height);
-			Log.d("DATA SIZE:", data.length +"");
 			
-			YuvImage yuvimage = new YuvImage(data, ImageFormat.NV21, width, height, null);			
-			long t2 = System.currentTimeMillis();
-
-			yuvimage.compressToJpeg(rect, 50, outstr);
-			long t3 = System.currentTimeMillis();
-
-			String encodedImage = Base64.encodeToString(outstr.toByteArray(),Base64.DEFAULT);
-			long t4 = System.currentTimeMillis();
-
-
-
-			Log.d("TIMES - ENCODEE", "" + (t4-t3));
-			Log.d("TIMES - COMPRRE", "" + (t3-t2));
-			Log.d("TIMES - YUVIMAG", "" + (t2-t1));
-			
-			
+			YuvImage yuvimage = new YuvImage(data, ImageFormat.NV21, width, height, null);	
+			yuvimage.compressToJpeg(rect, 50, outstr);		
+			String encodedImage = Base64.encodeToString(outstr.toByteArray(),Base64.DEFAULT);			
 			channel.send(encodedImage);			
 		}
-	}
-	
-	/**
-	 * Converts YUV420 NV21 to RGB8888
-	 * 
-	 * @param data byte array on YUV420 NV21 format.
-	 * @param width pixels width
-	 * @param height pixels height
-	 * @return a RGB8888 pixels int array. Where each int is a pixels ARGB. 
-	 */
-	public static int[] convertYUV420_NV21toRGB8888(byte [] data, int width, int height) {
-	    int size = width*height;
-	    int offset = size;
-	    int[] pixels = new int[size];
-	    int u, v, y1, y2, y3, y4;
-
-	    // i percorre os Y and the final pixels
-	    // k percorre os pixles U e V
-	    for(int i=0, k=0; i < size; i+=2, k+=2) {
-	        y1 = data[i  ]&0xff;
-	        y2 = data[i+1]&0xff;
-	        y3 = data[width+i  ]&0xff;
-	        y4 = data[width+i+1]&0xff;
-
-	        u = data[offset+k  ]&0xff;
-	        v = data[offset+k+1]&0xff;
-	        u = u-128;
-	        v = v-128;
-	        	        
-	        pixels[i  ] = convertYUVtoRGB(y1, u, v);
-	        pixels[i+1] = convertYUVtoRGB(y2, u, v);
-	        pixels[width+i  ] = convertYUVtoRGB(y3, u, v);
-	        pixels[width+i+1] = convertYUVtoRGB(y4, u, v);
-
-	        if (i!=0 && (i+2)%width==0)
-	            i+=width;
-	    }
-
-	    return pixels;
-	}
-
-	private static int convertYUVtoRGB(int y, int u, int v) {
-	    int r,g,b;
-
-	    r = y + (int)1.402f*v;
-	    g = y - (int)(0.344f*u +0.714f*v);
-	    b = y + (int)1.772f*u;
-	    r = r>255? 255 : r<0 ? 0 : r;
-	    g = g>255? 255 : g<0 ? 0 : g;
-	    b = b>255? 255 : b<0 ? 0 : b;
-	    return 0xff000000 | (b<<16) | (g<<8) | r;
 	}
 	
 
--- src/inviso/app/PanelActivity.java
+++ src/inviso/app/PanelActivity.java
@@ -17,6 +17,7 @@ import android.os.Message;
 import android.app.Activity;
 import android.content.Intent;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.Menu;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -28,34 +29,34 @@ import android.widget.TextView;
 public class PanelActivity extends IOIOActivity {
 	ConnectionChannel data;
 	ConnectionChannel video;
-	
+
 	MyLooper looper;
+	CameraPreview camPreview;
 
 	@Override
 	protected IOIOLooper createIOIOLooper() {
 		looper = new MyLooper();
 		return looper;
 	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_panel);
 
-		final TextView txtServer = (TextView)findViewById(R.id.txtServer); 
-		
-		/*
-		SurfaceView camView = new SurfaceView(this);
-		SurfaceHolder camHolder = camView.getHolder();
-		int width = 640;
-		int height = 480;
+		final TextView txtServer = (TextView) findViewById(R.id.txtServer);
 
-		final CameraPreview camPreview = new CameraPreview(width, height);
+		SurfaceView camView = new SurfaceView(this);
+		final SurfaceHolder camHolder = camView.getHolder();
+		final int width = 640;
+		final int height = 480;
+
+		camPreview = new CameraPreview(width, height);
 		camHolder.addCallback(camPreview);
 
 		FrameLayout mainLayout = (FrameLayout) findViewById(R.id.videoview);
 		mainLayout.addView(camView, new LayoutParams(width, height));
-        */
+
 		Intent intent = getIntent();
 		final String server = intent.getStringExtra("server");
 		final String user = intent.getStringExtra("user");
@@ -66,22 +67,41 @@ public class PanelActivity extends IOIOActivity {
 			public void run() {
 				data = new ConnectionChannel(server, ConnectionChannel.DATA_PORT, user, pass);
 				video = new ConnectionChannel(server, ConnectionChannel.VIDEO_PORT, user, pass);
-				// camPreview.setChannel(video);
+				camPreview.setChannel(video);
 
 				data.setCallback(new MessageCallback() {
 					@Override
 					public void callback(final char message, final char value) {
-						if ( looper != null){
-							if ( message == 101) looper.updateCommand(Direction.FORWARD);
-							if ( message == 102) looper.updateCommand(Direction.REVERSE);
-							if ( message == 103) looper.updateCommand(Direction.LEFT);
-							if ( message == 104) looper.updateCommand(Direction.RIGHT);								
-							if ( message == 105) looper.updateCommand(Direction.STOP);
+						Log.d("Message Callback", (int) message + ", " + (int) value);
+
+						if (camPreview != null) {
+							if (message == 30) {
+								camPreview.startCamera();
+								camHolder.addCallback(camPreview);
+							}
+							if (message == 31) {
+								camPreview.stopCamera();
+								camHolder.removeCallback(camPreview);
+							}
 						}
-						runOnUiThread(new Thread(){
+
+						if (looper != null) {
+							if (message == 10)
+								looper.updateCommand(Direction.FORWARD);
+							if (message == 11)
+								looper.updateCommand(Direction.REVERSE);
+							if (message == 12)
+								looper.updateCommand(Direction.LEFT);
+							if (message == 13)
+								looper.updateCommand(Direction.RIGHT);
+							if (message == 14)
+								looper.updateCommand(Direction.STOP);
+						}
+
+						runOnUiThread(new Thread() {
 							@Override
 							public void run() {
-								txtServer.setText("MESSAGE: " + (int)message +  " VALUE: " + (int)value);
+								txtServer.setText("MESSAGE: " + (int) message + " VALUE: " + (int) value);
 							}
 						});
 					}
@@ -91,7 +111,10 @@ public class PanelActivity extends IOIOActivity {
 
 	}
 
-	public void stupidButtonClicked(View w) {
-		data.send("Hello there: " + System.currentTimeMillis());
+	public void btnLogoutClicked(View w) {
+		this.finish();
+		Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		startActivity(intent);
 	}
 }
